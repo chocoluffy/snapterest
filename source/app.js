3fcd258e72dba38d06f078349530d8a853e6b497
chocoluffy <yushunzhe951104@gmail.com>
2015-09-30 23:35:38 -0400

    Create React node

---
+++ source/app.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var reactElement = React.createElement('div');
+ReactDOM.render(reactElement, document.getElementById('react-application'));
