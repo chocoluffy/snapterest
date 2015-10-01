946e1165f6c3eb1819fd6195d422aa378d04260c
chocoluffy <yushunzhe951104@gmail.com>
2015-09-30 22:56:01 -0400

    First .html file

---
+++ react学习笔记.js
@@ -0,0 +1,5 @@
+1. node.js 用client-side language 写server-side 的applications
+2. npm, node.js package manager.
+3. a node.js application is called a package, and each node.js package has a
+package.json file, containing the metadata associated with that package. 
+4. virtual DOM
